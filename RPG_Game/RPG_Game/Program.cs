bb2529fc59cda5d36736dbd10bda1b225c506018
Bíró Tamás Attila <birotamas20@gmail.com>
2024-02-14 00:48:17 +0100

    Add files via upload

--- RPG_Game/RPG_Game/Program.cs
+++ RPG_Game/RPG_Game/Program.cs
@@ -5,3 +5,11 @@ int rendery = 25;
 
 int x = 250;
 int y = 100;
+
+string[,] map1 = new string[1000+renderx, 1000+rendery];
+Console.SetWindowSize(Console.LargestWindowWidth, Console.LargestWindowHeight);
+Console.SetBufferSize(Console.LargestWindowWidth, Console.LargestWindowHeight);
+Console.CursorVisible = false;
+Console.Title = "RPG Game";
+Console.OutputEncoding = Encoding.UTF8;
+Console.ReadKey();
