fecbdf28e426aaab120c98f75c32b10092eb0cfb
Bíró Tamás Attila <birotamas20@gmail.com>
2024-02-14 00:51:09 +0100

    map beolvasása txtből

--- Program.cs
+++ Program.cs
@@ -13,3 +13,54 @@ Console.CursorVisible = false;
 Console.Title = "RPG Game";
 Console.OutputEncoding = Encoding.UTF8;
 Console.ReadKey();
+
+
+
+
+void mapload()
+{
+    for (int i = 0; i < 1000+renderx*4; i++)
+    {
+        for (int j = 0; j < 1000+rendery*4; j++)
+        {
+            map1[i, j] = " ";
+        }
+    }
+    
+    string[] lines = System.IO.File.ReadAllLines(@"map.txt");
+    for (int i = 0; i < 1000; i++)
+    {
+        for (int j = 0; j < 1000; j++)
+        {
+            map1[i+renderx*3, j+rendery*3] = lines[i][j].ToString();
+
+        }
+    }
+}
+
+map1[x, y] = "X";
+
+void renderMap()
+{
+    string map = "";
+    for (int i = x - rendery; i < x + rendery; i++)
+    {
+        for (int j = y - renderx; j < y + renderx; j++)
+        {
+            map += map1[i, j];
+        }
+        map += "\n";
+    }
+    
+}
+
+
+
+
+mapload();
+while (true)
+{
+    Console.Clear();
+    renderMap();
+    Thread.Sleep(1);
+}
