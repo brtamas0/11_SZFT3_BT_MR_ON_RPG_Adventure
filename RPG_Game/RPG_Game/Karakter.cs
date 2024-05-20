e265b4d8d0059f45622f9620de40de15f990ffb4
Tomy <birotamas20@gmail.com>
2024-05-21 01:37:19 +0200

    Karakter osztály elkészítve
    MaxHp maybe useless

--- RPG_Game/RPG_Game/Karakter.cs
+++ RPG_Game/RPG_Game/Karakter.cs
@@ -1,12 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RPG_Game
+﻿namespace RPG_Game
 {
-    internal class Class1
+    public class Karakter
     {
+        public int x { get; set; }
+        public int y { get; set; }
+        public string Nev { get; set; }
+        public int Sebzes { get; set; }
+        public int Armor { get; set; }
+        public int Xp { get; set; }
+        public int Level { get; set; }
+        public int Hp { get; set; }
+        public int MaxHp { get; set; } //maybe nem lesz értelme?
+        public int Gold { get; set; }
+        public string[] Inventory { get; set; }
+
+        public string[] Kinezet { get; private set; } = {
+            "   .-.   ",
+            " __|=|__ ",
+            "(_/`-`\\_)",
+            "//\\___/\\\\",
+            "<>/   \\<>",
+            " \\|_._|/ ",
+            "  <_I_>  ",
+            "   |||   ",
+            "  /_|_\\  "
+        };
+
+        public Karakter(int x, int y, string nev, int sebzes, int armor, int xp, int level, int hp, int gold, string[] inventory)
+        {
+            this.x = x;
+            this.y = y;
+            this.Nev = nev;
+            this.Sebzes = sebzes;
+            this.Armor = armor;
+            this.Xp = xp;
+            this.Level = level;
+            this.Hp = hp;
+            this.MaxHp = hp;
+            this.Gold = gold;
+            this.Inventory = inventory;
+        }
     }
 }
