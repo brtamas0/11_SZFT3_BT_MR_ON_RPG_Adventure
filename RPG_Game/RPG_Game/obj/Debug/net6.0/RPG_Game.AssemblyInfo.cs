6ecd79d50082a5aaeb3357f7e138f7107dc393d4
Tomy <birotamas20@gmail.com>
2024-05-28 01:34:35 +0200

    Map kiírás refakt

--- RPG_Game/RPG_Game/obj/Debug/net6.0/RPG_Game.AssemblyInfo.cs
+++ RPG_Game/RPG_Game/obj/Debug/net6.0/RPG_Game.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RPG_Game")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a7d76ff92c866a053589707736d4adf70c86449")]
 [assembly: System.Reflection.AssemblyProductAttribute("RPG_Game")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RPG_Game")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
