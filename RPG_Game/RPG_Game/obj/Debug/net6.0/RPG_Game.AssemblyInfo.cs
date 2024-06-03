8a8854614f02bf8d2a6a2a74b60ee54b7ebeba95
MRoland05 <roland.marodi@gmail.com>
2024-06-03 21:21:34 +0200

    Revert "Javítás"
    This reverts commit dbc38e09283b482dc0064ad1d46fcce8f781e638.

--- RPG_Game/RPG_Game/obj/Debug/net6.0/RPG_Game.AssemblyInfo.cs
+++ RPG_Game/RPG_Game/obj/Debug/net6.0/RPG_Game.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RPG_Game")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12cc246875327158f4ddaefdc9263c231fa20aa7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e2e1834a13fdfece4ff0176cd682095cd266de2")]
 [assembly: System.Reflection.AssemblyProductAttribute("RPG_Game")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RPG_Game")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
